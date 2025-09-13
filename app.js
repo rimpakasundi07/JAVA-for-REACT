464b5a7bc3a75de0585caca58f8e5c1110e40c63
rimpa kasundi <rimpakasundi@gmail.com>
2025-09-13 21:11:34 +0530

    JavaScript Array Operations Demo

--- app.js
+++ app.js
@@ -19,9 +19,22 @@
 // }
 
 // ............-------------------------->
-const num = 40;
-if (num >= 20 && num <= 30) {
-  console.log("yes");
-} else {
-  console.log("no");
-}
+
+// const num = 40;
+// if (num >= 20 || num <= 30) {
+//   console.log("yes");
+// } else {
+//   console.log("no");
+// }
+
+// ............-------------------------->
+// .ARRAY
+// const friends = ["hero", "SRK", "PW", "ARS"];
+// friends.push("BJ");
+// friends.pop();
+// // - first e add howar joono-->
+// friends.unshift("KK");
+// // - first e bad howar joono-->
+// friends.shift("KK");
+// console.log(friends.length);
+// -------------------------------->>>>>>>>>>>>
