4efc876c8dcd34a5b11ffde31cb01192e17c6675
rimpa kasundi <rimpakasundi@gmail.com>
2025-09-13 22:11:38 +0530

    add some demos of array function

--- tamplet.js
+++ tamplet.js
@@ -1,7 +1,27 @@
-const fName = "Rimpa";
-const lName = "kasundi";
+// const fName = "Rimpa";
+// const lName = "kasundi";
 
-console.log(`I am RK ${fName} ${lName} `);
+// console.log(`I am RK ${fName} ${lName} `);
+
 //output: I am Rimpa Kasundi
 
 // -------------------------------->>>>>>>>
+// Array function -- >>>
+
+// function sum() {
+//   confirm.log(arguments);
+// }
+
+// ------------------------>>
+
+// const sum = (a, b, c) => a + b + c;
+
+// const result = sum(1, 2, 3);
+// console.log(result);
+
+// ------------------------>>
+
+// const sum = (...rest) => {
+//   console.log(rest);
+// };
+// sum(1, 2, 3);
