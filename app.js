38b21f93adc2abd1b83a1a8465013a07b70a5896
rimpa kasundi <rimpakasundi@gmail.com>
2025-09-13 21:28:50 +0530

    JS Functions Demo

--- app.js
+++ app.js
@@ -38,3 +38,33 @@
 // friends.shift("KK");
 // console.log(friends.length);
 // -------------------------------->>>>>>>>>>>>
+
+// const friends = ["hero", "SRK", "PW", "ARS"];
+// console.log(friends.slice(2, 3));
+
+// ---------------------------------------------------->?>>
+
+// const friends = ["hero", "SRK", "PW", "ARS"];
+// console.log(friends.splice(2, 3));
+
+// ---------------------------------------------------->>>>
+///// LOOP***
+// for (let index = 0; index < friends.length; index++) {
+//   const element = friends[index];
+//   console.log(element);
+// }
+
+// ------------------------------------------------------->
+
+// . FUNCTION ****
+
+// function sum(a, b) {
+//   console.log(arguments);
+//   const result = a + b;
+//   console.log(result);
+// }
+
+// const output = sum(4, 5);
+// console.log(output);
+
+// ------------------------------------->>
