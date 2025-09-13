6a8f482cd13b3338b307a632c9a9400916a5a44a
rimpa kasundi <rimpakasundi@gmail.com>
2025-09-13 21:00:00 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+//. variable-------------------->
+
+// var
+
+//. let and Const----------------------->
+
+// let name = "RIMPA";
+// name = "Rimpa Kasundi";
+
+// const countryName = "INDIA";
+
+// .if else------------------------->
+
+// const num = 20;
+// if (num > 20) {
+//   console.log("yes");
+// } else {
+//   console.log("no");
+// }
+
+// ............-------------------------->
+const num = 40;
+if (num >= 20 && num <= 30) {
+  console.log("yes");
+} else {
+  console.log("no");
+}
