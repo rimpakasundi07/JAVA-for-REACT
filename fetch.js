06eeccd5349e17817e21151aaf703c76e8b9d7f8
rimpa kasundi <rimpakasundi@gmail.com>
2025-09-14 20:46:55 +0530

    extract object keys and values using Object methods

--- fetch.js
+++ fetch.js
@@ -1,3 +1,28 @@
+// const person = {
+//   name: "Rimpa",
+//   age: 19,
+//   friends: ["Raja", "Arghya", "Rudra"],
+//   family: {
+//     fathername: "Roky",
+//     mothername: "Rupa",
+//   },
+// };
+
+// const jsonData = JSON.stringify(person);
+// ///// . console.log(jsonData);
+
+// const planData = JSON.parse(jsonData);
+// console.log(planData);
+// ---------------------------------------------------->
+
+// --      FETCH             **
+
+// fetch("url")
+//   .then(res > res.json())
+//   .then((data) => console.log(""));
+
+// ---------------------------------------------------->
+
 const person = {
   name: "Rimpa",
   age: 19,
@@ -8,8 +33,7 @@ const person = {
   },
 };
 
-const jsonData = JSON.stringify(person);
-// . console.log(jsonData);
+const keys = Object.keys(person);
 
-const planData = JSON.parse(jsonData);
-console.log(planData);
+const values = Object.values(person);
+console.log(values);
