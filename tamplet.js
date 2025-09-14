ff068e31729139c8913d8b8e808407359cbc9ca6
rimpa kasundi <rimpakasundi@gmail.com>
2025-09-14 11:34:43 +0530

    add some demos of spred oparator

--- tamplet.js
+++ tamplet.js
@@ -25,3 +25,14 @@
 //   console.log(rest);
 // };
 // sum(1, 2, 3);
+
+// -------------------------------->>
+//spred oparator
+// const numbers = [1, 2, 3, 4, 5, 6];
+// console.log(...numbers);
+
+// -------------------------------->>
+
+// const numbers = [1, 2, 3, 4, 5, 6];
+// const newNumbers = [...numbers, 10, 100];
+// console.log(newNumbers);
