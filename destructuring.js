6bd4effb3288024ebb66a2a4071f4ee5966fca5d
rimpa kasundi <rimpakasundi@gmail.com>
2025-09-14 17:22:51 +0530

    add array destructuring exaples

---
+++ destructuring.js
@@ -0,0 +1,13 @@
+// array destructuring
+
+// const friends = ["hero", "heroine", "Juni"];
+
+// const element1 = friends[0];
+
+// const element2 = friends[1];
+
+// const element3 = friends[2];
+
+// console.log(element1, element2, element3);
+
+// --------------------------------------------------->
