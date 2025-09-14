1e60aea3ccf97f30b04545a5b7c7c24b460a24b4
rimpa kasundi <rimpakasundi@gmail.com>
2025-09-14 21:56:03 +0530

    demonstrate truthy and falsy values in JavaScript

---
+++ shortcut.js
@@ -0,0 +1,17 @@
+// false value
+// 0 , -0 , false , null , undefined ,
+
+/// true value
+// "huikoiedjoioi", 52163420572 , {}, [] , true
+
+//1.
+const test = 5;
+
+if (test) {
+  console.log("its truth");
+} else {
+  console.log("its false");
+}
+
+// 2.    ***
+test ? console.log("its truth") : console.log("its false");
