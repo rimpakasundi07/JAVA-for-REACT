f14ebb60be5a8d552fbc57c9b13f08ecb133a956
rimpa kasundi <rimpakasundi@gmail.com>
2025-09-15 11:13:42 +0530

    Toggle user visibility based on active state using ternary and logical operators

--- shortcut.js
+++ shortcut.js
@@ -5,13 +5,31 @@
 // "huikoiedjoioi", 52163420572 , {}, [] , true
 
 //1.
-const test = 5;
+// const test = 5;
 
-if (test) {
-  console.log("its truth");
-} else {
-  console.log("its false");
-}
+// if (test) {
+//   console.log("its truth");
+// } else {
+//   console.log("its false");
+// }
 
-// 2.    ***
-test ? console.log("its truth") : console.log("its false");
+// // 2.    ***
+// test ? console.log("its truth") : console.log("its false");
+
+// ------------------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// --- similar of 2. -----
+// const num = 10;
+// const result = num >= 10 && num < 20 ? "true" : "false";
+// console.log(result);
+
+// const isActive = true;
+// console.log(!isActive);
+
+// ----------------------------------------------
+
+const isActive = true;
+const showUser = () => console.log("show user green");
+const hideUser = () => console.log("hide user");
+// . isActive ? showUser() : hideUser();
+isActive && showUser();
+// .isActive || showUser();
